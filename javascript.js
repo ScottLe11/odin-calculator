384ed12893cced31913ddee580cfbb877cf83e30
Scott <scottle220044@gmail.com>
2025-09-17 22:33:28 -0700

    implement basic format of calculator and basic operator formulas

---
+++ javascript.js
@@ -0,0 +1,45 @@
+let num1;
+let num2;
+let op;
+
+//Basic Math formulas
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+
+function operate(op, num1, num2){
+    if (op == '+'){
+        return add(num1, num2);
+    }
+    else if (op == '-'){
+        return subtract(num1, num2);
+    }
+    else if (op == '*'){
+        return multiply(num1, num2);
+    }
+    else if (op == '/'){
+        return divide(num1, num2);
+    }
+}
+
+//Clicking buttons
+const allBoxes = document.querySelectorAll(".box");
+allBoxes.forEach(box => {
+    box.addEventListener("click", (e) => {
+        console.log(e.target.textContent);
+    });
+});
+
