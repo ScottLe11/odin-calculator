8e9b3904e506a1eb6500904b0a91225faa6146cd
Scott <scottle220044@gmail.com>
2025-09-19 12:16:30 -0700

    Reworked parsing expression into variables and fixed the basic display of operations

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-let num1;
-let num2;
-let op;
+let operand1 = null;
+let operand2 = null;
+let op = null;
 
 const operators = ['+', '-', '*', '/'];
 
@@ -39,8 +39,42 @@ function operate(op, num1, num2){
 
 
 
-function storeExpression(expr){
-    return expr.split(/([+\-*/])/);
+// function storeExpression(expr){
+//     return expr.split(/([+\-*/])/);
+// }
+
+function handleNum(val){
+    if (op == null){
+        // build first operand
+        if (operand1 == null){
+            operand1 = val;
+        }
+        else{
+            operand1 += val;
+        }
+        divBox.textContent = operand1;
+    }
+    else{
+        if (operand2 == null){
+            operand2 = val;
+        }
+        else{
+            operand2 += val;
+        }
+        divBox.textContent = operand2;
+    }
+}
+
+function handleOperator(val){
+    op = val;
+    divBox.textContent = op;
+}
+
+function handleEquals(){
+    if (operand1 != null && operand2 != null && op != null){
+        console.log(operand1 + op + operand2);
+        divBox.textContent = operate(op, Number(operand1), Number(operand2));
+    }
 }
 
 
@@ -49,19 +83,48 @@ const divBox = document.querySelector(".inputBox");
 const allBoxes = document.querySelectorAll(".box");
 allBoxes.forEach(box => {
     box.addEventListener("click", (e) => {
+        const val = e.target.textContent;
 
-        //calculate
-        if (box.textContent == "="){
-            const parts = storeExpression(divBox.textContent);
-            if (parts.length == 3 && (!isNaN(Number(parts[0])))
-            && (!isNaN(Number(parts[2]))) && operators.includes(parts[1])){
-                divBox.textContent = operate(parts[1], Number(parts[0]), Number(parts[2]));
-            }
+        if (!isNaN(Number(val))){
+            handleNum(val);
         }
-        else{
-            divBox.textContent += e.target.textContent;
-            console.log(e.target.textContent);
+
+        else if (operators.includes(val)){
+            handleOperator(val);
         }
+        else if (val == "="){
+            handleEquals();
+        }
+        else if (val = "c"){
+            handleErase();
+        }
+        // const parts = storeExpression(divBox.textContent);
+        // console.log(parts);
+        // //calculate
+        // if (box.textContent == "="){
+        //     //const parts = storeExpression(divBox.textContent);
+            
+        //     if (parts.length == 3 && (!isNaN(Number(parts[0])))
+        //     && (!isNaN(Number(parts[2]))) && operators.includes(parts[1])){
+        //         divBox.textContent = operate(parts[1], Number(parts[0]), Number(parts[2]));
+        //     }
+        //     //else if (parts.length == 4){
+
+        //     //}
+        // }
+        // else if (parts.length == 4){
+        //     console.log("hjere");
+        //     //const parts = storeExpression(divBox.textContent);
+        //     if ((!isNaN(Number(parts[0])))
+        //     && (!isNaN(Number(parts[2]))) && operators.includes(parts[1]) && operators.includes(parts[3])){
+        //         divBox.textContent = operate(parts[1], Number(parts[0]), Number(parts[2])) + parts[3];
+        //     }
+        // }
+
+        // else{
+        //     divBox.textContent += e.target.textContent;
+        //     console.log(e.target.textContent);
+        // }
     });
 });
 
