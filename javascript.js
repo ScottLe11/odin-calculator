03f8ccac1c08c5dc3304a1471ab6cec7984c7c90
Scott <scottle220044@gmail.com>
2025-09-18 19:21:13 -0700

    implemented equals button and display result of operation in div box

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ let num1;
 let num2;
 let op;
 
+const operators = ['+', '-', '*', '/'];
+
 //Basic Math formulas
 function add(num1, num2){
     return num1 + num2;
@@ -35,13 +37,31 @@ function operate(op, num1, num2){
     }
 }
 
+
+
+function storeExpression(expr){
+    return expr.split(/([+\-*/])/);
+}
+
+
 const divBox = document.querySelector(".inputBox");
 //Clicking buttons
 const allBoxes = document.querySelectorAll(".box");
 allBoxes.forEach(box => {
     box.addEventListener("click", (e) => {
-        divBox.textContent += e.target.textContent;
-        console.log(e.target.textContent);
+
+        //calculate
+        if (box.textContent == "="){
+            const parts = storeExpression(divBox.textContent);
+            if (parts.length == 3 && (!isNaN(Number(parts[0])))
+            && (!isNaN(Number(parts[2]))) && operators.includes(parts[1])){
+                divBox.textContent = operate(parts[1], Number(parts[0]), Number(parts[2]));
+            }
+        }
+        else{
+            divBox.textContent += e.target.textContent;
+            console.log(e.target.textContent);
+        }
     });
 });
 
